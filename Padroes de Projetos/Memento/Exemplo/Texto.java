c95e9cfe00aa04ff4a5cdc7649d14ea7edbd3a70
victor-santana <61338839+victor-santana@users.noreply.github.com>
2020-11-08 15:33:28 -0300

    Add files via upload

---
+++ Padroes de Projetos/Memento/Exemplo/Texto.java
@@ -0,0 +1,22 @@
+public class Texto {
+    protected String texto;
+    TextoCareTaker caretaker;
+ 
+    public Texto() {
+        caretaker = new TextoCareTaker();
+        texto = new String();
+    }
+ 
+    public void escreverTexto(String novoTexto) {
+        caretaker.adicionarMemento(new TextoMemento(texto));
+        texto += novoTexto;
+    }
+ 
+    public void desfazerEscrita() {
+        texto = caretaker.getUltimoEstadoSalvo().getTextoSalvo();
+    }
+ 
+    public void mostrarTexto() {
+        System.out.println(texto);
+    }
+}
